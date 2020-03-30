5d58dcba20f5b400f36cb3561caf19b5b0e88a3e
letscode <roman@ukrainiansoftware.com>
2020-03-30 15:14:59 +0300

    api v2 test release

---
+++ upload/admin/language/en-gb/extension/payment/coinpayments.php
@@ -0,0 +1,35 @@
+<?php
+$_['heading_title'] = 'CoinPayments.net Cryptocoin Payments';
+
+$_['text_payment'] = 'Payments';
+
+$_['text_coinpayments'] = '<a onclick="window.open(\'https://www.coinpayments.net/\');"><img src="view/image/payment/coinpayments.png" alt="CoinPayments" title="coinpayments" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_edit'] = 'Edit CoinPayments Settings';
+$_['text_success'] = 'Your account details have been updated!';
+$_['text_all_zones'] = 'All Zones';
+
+
+$_['tab_general'] = 'General';
+$_['tab_order_status'] = 'Order Status';
+
+
+$_['entry_client_id'] = 'Client ID:';
+$_['entry_client_secret'] = 'Client Secret:';
+$_['entry_webhooks'] = 'Webhooks:';
+$_['entry_cancelled_status'] = 'Cancelled/Timed Out Status:';
+$_['entry_completed_status'] = 'Completed Status:';
+$_['entry_pending_status'] = 'Pending Status:';
+$_['entry_geo_zone'] = 'Geo Zone:';
+$_['entry_status'] = 'Status:';
+$_['entry_sort_order'] = 'Sort Order:';
+
+$_['help_webhooks'] = "Enable to use CoinPayments.NET webhooks.";
+$_['help_client_id'] = "Found on your CoinPayments.NET merchant page.";
+$_['help_client_secret'] = "Found on your CoinPayments.NET merchant page when you create a new client.";
+
+
+$_['warning_permission'] = 'Warning: You do not have permission to modify the CoinPayments payment module.';
+
+$_['error_client_id'] = 'You must specify your Client ID';
+$_['error_client_secret'] = 'You must specify your CoinPayments.Net Client Secret';
+$_['error_invalid_credentials'] = 'Your CoinPayments.Net credentials invalid';
